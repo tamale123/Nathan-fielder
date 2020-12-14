f2b783b506142766dfba3c92bcbad2e922277343
tamale123 <61068363+tamale123@users.noreply.github.com>
2020-12-14 09:31:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 import random
 TOKEN = 'Nzg3NTc1MDA1MTkzOTYxNTEz.X9W8YQ.ZJpfsuNX0pfzzPmD0itbCq9WkE4'
 client = discord.Client()
-greet = ["hi","hello","helo","hey","sup","hiiii",]
+greet = ["hi","hello","helo","hey","sup","hiiii","yo"]
 ness = ["*vomits on carpet* oh sorry. I was just so disgusted because you said ness","god I hate ness","ness >:(",'"ness"... every time someone says his name god weeps.',"""top 5 reasons I hate ness mains:
 1: they main ness
 2: they main ness
@@ -13,15 +13,26 @@ ness = ["*vomits on carpet* oh sorry. I was just so disgusted because you said n
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return
+        return outer
+    if "nathan" in message.content.lower or "nate" in message.content.lower
+        msg = message.content.lower().replace("nate",message.author)
+        msg = msg.replace("nathan",message.author)
+        await message.channel.send(msg)
+        return outer
     for ele in greet:
         if ele in message.content.lower():
             await message.channel.send(random.choice(greet))
+            return outer
     msg = message.content.lower().replace("'","")
     if "im " in msg:
         msg = 'Hi '+(msg[(msg.find("im ")+3):])+", I'm dad."
         await message.channel.send(msg)
+        return outer
     if "ness" in message.content.lower():
         await message.channel.send(random.choice(ness))
-
+        return outer
+    if "i love you" in message.content.lower():
+        await message.channel.send("ily too :)")
+        return outer
+    
 client.run(TOKEN)

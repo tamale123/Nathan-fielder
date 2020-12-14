e1702b38f5e5b56ffbd85631512a1a50209ee0b1
tamale123 <61068363+tamale123@users.noreply.github.com>
2020-12-14 09:47:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,19 @@ ness = ["*vomits on carpet* oh sorry. I was just so disgusted because you said n
 5: they main ness""","god I hate ness","ness is the worse","I wish ness die","ness is a stupid dumb diaper baby","hey guys can we please stop talking about ness? I really hate his guts and I cry every time I read his name."]
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return outer
-    if "nathan" in message.content.lower or "nate" in message.content.lower:
-        msg = message.content.lower().replace("nate",message.author)
-        msg = msg.replace("nathan",message.author)
-        await message.channel.send(msg)
-        return outer
-    for ele in greet:
-        if ele in message.content.lower():
-            await message.channel.send(random.choice(greet))
-            return outer
-    msg = message.content.lower().replace("'","")
-    if "im " in msg:
-        msg = 'Hi '+(msg[(msg.find("im ")+3):])+", I'm dad."
-        await message.channel.send(msg)
-        return outer
-    if "ness" in message.content.lower():
-        await message.channel.send(random.choice(ness))
-        return outer
-    if "i love you" in message.content.lower():
-        await message.channel.send("ily too :)")
-        return outer
-    
+    unless message.author == client.user:
+        if "nathan" in message.content.lower or "nate" in message.content.lower:
+            msg = message.content.lower().replace("nate",message.author).replace("nathan",message.author)
+            await message.channel.send(msg)
+        for ele in greet:
+            if ele in message.content.lower():
+                await message.channel.send(random.choice(greet))
+        if "im " in message.content.lower().replace("'",""):
+            msg = 'Hi '+(msg[(msg.find("im ")+3):])+", I'm dad."
+            await message.channel.send(msg)
+        if "ness" in message.content.lower():
+            await message.channel.send(random.choice(ness))
+        if "i love you" in message.content.lower():
+            await message.channel.send("ily too :)")
+
 client.run(TOKEN)

c7a3d263e38e68a3056e042cdf2f0c0ece1b9f73
tamale123 <61068363+tamale123@users.noreply.github.com>
2020-12-14 10:44:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ async def on_message(message):
             await message.channel.send(random.choice(greet))
     for b in name:
         if b in message.content.lower():
-            await message.channel.send(message.content.lower().replace("nate",message.author).replace("nathan",message.author))
+            await message.channel.send(b)
+            await message.channel.send(message.content.lower().replace(b,message.author))
     msg = message.content.lower().replace("'","")
     if "im " in msg:
         msg = 'Hi '+(msg[(msg.find("im ")+3):])+", I'm dad."

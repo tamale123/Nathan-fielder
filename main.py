2b68c2bcfe6113f36578e4ef633f4559fe473250
tamale123 <61068363+tamale123@users.noreply.github.com>
2020-12-14 20:55:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -45,11 +45,11 @@ async def on_message(message):
             await message.channel.send("b")
             for x in q2:
                 if x in msg:
-                    await message.channel.send("h")
+                    await message.channel.send("ha")
                     msg = (msg[(msg.find(x)):])
                     sch = w.search(msg, limit=1)
                     sch = str(sch).split("'")
-                    await message.channel.send(sch)
+                    await message.channel.send(sch[1])
                     img = w.getImageByPageName.source(sch)
                     msg = sch.summary
                     await message.channel.send(msg)

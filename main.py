dc651fe49000a651aff84c17b091b1b13d0bffb2
tamale123 <61068363+tamale123@users.noreply.github.com>
2020-12-15 18:21:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,48 +21,49 @@ ness = ["*vomits on carpet* oh sorry. I was just so disgusted because you said n
 5: they main ness""","god I hate ness","ness is the worse","I wish ness die","ness is a stupid dumb diaper baby","hey guys can we please stop talking about ness? I really hate his guts and I cry every time I read his name."]
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    for ele in greet:
-        if ele in message.content.lower() or ele.strip() == message.content.lower():
-            await message.channel.send(random.choice(greet))
-    for b in name:
-        if b in message.content.lower():
-            msg = message.content.lower().replace(b,(str(message.author).split("#", 1)[0]))
+    try:
+        if message.author == client.user:
+            return
+        for ele in greet:
+            if ele in message.content.lower() or ele.strip() == message.content.lower():
+                await message.channel.send(random.choice(greet))
+        for b in name:
+            if b in message.content.lower():
+                msg = message.content.lower().replace(b,(str(message.author).split("#", 1)[0]))
+                await message.channel.send(msg)
+        msg = message.content.lower().replace("'","")
+        if "im " in msg:
+            msg = 'Hi '+(msg[(msg.find("im ")+3):])+", I'm dad."
             await message.channel.send(msg)
-    msg = message.content.lower().replace("'","")
-    if "im " in msg:
-        msg = 'Hi '+(msg[(msg.find("im ")+3):])+", I'm dad."
-        await message.channel.send(msg)
-    if "ness" in message.content.lower():
-        await message.channel.send(random.choice(ness))
-    if "no u" in message.content:
-        await message.channel.send("no u")
-    msg = message.content.lower()
-    c = 0
-    for x in q1:
-        if x in msg:
-            msg = (msg[(msg.find(x)):])
-            for x in q2:
-                if x in msg:
-                    msg = (msg[(msg.find(x)):])
-                    sch = msg.capitalize()
-                    sca = wiki_wiki.page(sch).exists
-                    if sca != True:
-                        sch = w.search(msg, limit=1)
-                        sch = str(sch).split("'")
-                        sch = sch[1]
-                    msg = wiki_wiki.page(sch).summary                   
-                    try:
-                        img = w.getImageByPageName(sch)
-                        img = (img["source"])
-                    except:
-                        img = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.pJWnPpkkd8SWkI42q1VY_AHaFS%26pid%3DApi&f=1"
-                    embedVar = discord.Embed(title=sch, description=msg[:350]+"[...]("+wiki_wiki.page(sch).fullurl+")", color=0x7289da, url=wiki_wiki.page(sch).fullurl)
-                    embedVar.set_footer(text=wiki_wiki.page(sch).fullurl[8:])
-                    embedVar.set_image(url=img)
-                    unless c==1:
-                        c = 1
-                        await message.channel.send(embed=embedVar)
-                    
+        if "ness" in message.content.lower():
+            await message.channel.send(random.choice(ness))
+        if "no u" in message.content:
+            await message.channel.send("no u")
+        msg = message.content.lower()
+        c = 0
+        for x in q1:
+            if x in msg:
+                msg = (msg[(msg.find(x)):])
+                for x in q2:
+                    if x in msg:
+                        msg = (msg[(msg.find(x)):])
+                        sch = msg.capitalize()
+                        sca = wiki_wiki.page(sch).exists
+                        if sca != True:
+                            sch = w.search(msg, limit=1)
+                            sch = str(sch).split("'")
+                            sch = sch[1]
+                        msg = wiki_wiki.page(sch).summary                   
+                        try:
+                            img = w.getImageByPageName(sch)
+                            img = (img["source"])
+                        except:
+                            img = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.pJWnPpkkd8SWkI42q1VY_AHaFS%26pid%3DApi&f=1"
+                        embedVar = discord.Embed(title=sch, description=msg[:350]+"[...]("+wiki_wiki.page(sch).fullurl+")", color=0x7289da, url=wiki_wiki.page(sch).fullurl)
+                        embedVar.set_footer(text=wiki_wiki.page(sch).fullurl[8:])
+                        embedVar.set_image(url=img)
+                        unless c==1:
+                            c = 1
+                            await message.channel.send(embed=embedVar)
+    except Exception as e: print(e)                
 client.run(TOKEN)

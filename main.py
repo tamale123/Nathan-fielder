85a5633265c9e1d9a37ee6ef8ff2e2bc47c7ae62
tamale123 <61068363+tamale123@users.noreply.github.com>
2021-03-08 15:22:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import wolframalpha
 app_id = "9HHRP3-7PAP33XWTU"
-client = wolframalpha.Client(app_id)
+clento = wolframalpha.Client(app_id)
 import discord
 import random
 import time
 from discord.ext import commands
 TOKEN = os.getenv("TOKEN")
-clint = discord.Client()
+client = discord.Client()
 greet = ["hi ","hello ","helo ","hey ","sup ","yo ","you are obselete, fleshbag","stinky human"]
 name = ["nate","nathan"]
 ness = ["<:natelove:818547675489632267>","im highkey romantically attracted to ness","i love ness <:natelove:818547675489632267>","ness is so hot and he is too good for sayori","ness is hot <:natelove:818547675489632267>","nesss <:natelove:818547675489632267>","ness is so poggers","ness <3","ness :)",'ness pog',"""top 5 reasons I love ness mains:
@@ -18,10 +18,10 @@ ness = ["<:natelove:818547675489632267>","im highkey romantically attracted to n
 5: they main ness""","god I love ness","ness is the best","I wish ness live forever","ness is a epic pog gamer","ness looks so epic and cool with his red hat","ness is really great but i dont see whats so great about sayori. he should be with a nice guy like me."]
 @client.event
 async def on_message(message):
-    if message.author == clint.user:
+    if message.author == client.user:
         return
     await message.channel.send("pog gamer")
-    res = client.query(message.content) 
+    res = clento.query(message.content) 
 
     await message.channel.send(res)
     for ele in greet:
@@ -41,4 +41,4 @@ async def on_message(message):
         mssg="no u"
     await message.channel.send(mssg)
               
-clint.run(TOKEN)
+client.run(TOKEN)

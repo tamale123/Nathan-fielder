1b181588551da295b838dd9a941871d721fac86f
tamale123 <61068363+tamale123@users.noreply.github.com>
2021-03-08 13:53:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,14 @@ import os
 import wolframalpha
 app_id = "9HHRP3-7PAP33XWTU"
 client = wolframalpha.Client(app_id)
-from wikipya.core import Wikipya
-import wikipediaapi
-w = Wikipya("en")
-wiki_wiki = wikipediaapi.Wikipedia('en')
 import discord
 import random
 import time
 from discord.ext import commands
 TOKEN = os.getenv("TOKEN")
 client = discord.Client()
-hr = ["hours","hrs","hr"]
-mn = ["minutes","min", "mins"]
-greet = ["hi ","hello ","helo ","hey ","sup ","hiiii ","yo ","you are obselete, fleshbag"]
+greet = ["hi ","hello ","helo ","hey ","sup ","yo ","you are obselete, fleshbag","stinky human"]
 name = ["nate","nathan"]
-q1 = ["what","who","wtf"]
-q2 = ["is","was","are","'s"]
 ness = ["<:natelove:818547675489632267>","im highkey romantically attracted to ness","i love ness <:natelove:818547675489632267>","ness is so hot and he is too good for sayori","ness is hot <:natelove:818547675489632267>","nesss <:natelove:818547675489632267>","ness is so poggers","ness <3","ness :)",'ness pog',"""top 5 reasons I love ness mains:
 1: they main ness
 2: they main ness
@@ -26,50 +18,26 @@ ness = ["<:natelove:818547675489632267>","im highkey romantically attracted to n
 5: they main ness""","god I love ness","ness is the best","I wish ness live forever","ness is a epic pog gamer","ness looks so epic and cool with his red hat","ness is really great but i dont see whats so great about sayori. he should be with a nice guy like me."]
 @client.event
 async def on_message(message):
+    res = client.query(message.content) 
+    answer = next(res.results).text  
+    mssg=(answer) 
     if message.author == client.user:
         return
     for ele in greet:
         if ele in message.content.lower() or ele.strip() == message.content.lower():
-            await message.channel.send(random.choice(greet))
+            mssg=random.choice(greet)
     for b in name:
         if b in message.content.lower():
             msg = message.content.lower().replace(b,(str(message.author).split("#", 1)[0]))
-            await message.channel.send(msg)
+            mssg=msg
     msg = message.content.lower().replace("'","")
     if "im " in msg:
         msg = 'Hi '+(msg[(msg.find("im ")+3):])+", I'm dad."
-        await message.channel.send(msg)
+        mssg=msg
     if "ness" in message.content.lower():
-        await message.channel.send(random.choice(ness))
+        mssg=random.choice(ness)
     if "no u" in message.content:
-        await message.channel.send("no u")
-    """msg = message.content.lower()
-    c = 0
-    for x in q1:
-        if x in msg:
-            msg = (msg[(msg.find(x)):])
-            for x in q2:
-                if x in msg:
-                    msg = (msg[(msg.find(x)):])
-                    sch = msg.capitalize()
-                    sca = wiki_wiki.page(sch).exists
-                    if sca != True:
-                        sch = w.search(msg, limit=1)
-                        sch = str(sch).split("'")
-                        sch = sch[1]
-                    msg = wiki_wiki.page(sch).summary                   
-                    try:
-                        img = w.getImageByPageName(sch)
-                        img = (img["source"])
-                    except:
-                        img = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.pJWnPpkkd8SWkI42q1VY_AHaFS%26pid%3DApi&f=1"
-                    embedVar = discord.Embed(title=sch, description=msg[:350]+"[...]("+wiki_wiki.page(sch).fullurl+")", color=0x7289da, url=wiki_wiki.page(sch).fullurl)
-                    embedVar.set_footer(text=wiki_wiki.page(sch).fullurl[8:])
-                    embedVar.set_image(url=img)
-                    if c == 0:
-                        c = 1
-                        await message.channel.send(embed=embedVar)"""
-    res = client.query(message.content) 
-    answer = next(res.results).text  
-    await message.channel.send(answer)           
+        mssg="no u"
+    await message.channrl.send(mssg)
+              
 client.run(TOKEN)

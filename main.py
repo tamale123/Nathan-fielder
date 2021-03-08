660bd39436a9b71c519323f894a6dac12a2a5e0a
tamale123 <61068363+tamale123@users.noreply.github.com>
2021-03-08 14:02:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ ness = ["<:natelove:818547675489632267>","im highkey romantically attracted to n
 5: they main ness""","god I love ness","ness is the best","I wish ness live forever","ness is a epic pog gamer","ness looks so epic and cool with his red hat","ness is really great but i dont see whats so great about sayori. he should be with a nice guy like me."]
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
+    await message.channel.send("pog gamer")
     res = client.query(message.content) 
     answer = next(res.results).text  
     mssg=(answer) 
-    if message.author == client.user:
-        return
     for ele in greet:
         if ele in message.content.lower() or ele.strip() == message.content.lower():
             mssg=random.choice(greet)

c926b6af7fc6d1dcbb178121461f3a560bdc9c2a
tamale123 <61068363+tamale123@users.noreply.github.com>
2021-03-08 15:51:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,8 @@ async def on_message(message):
     await message.channel.send("pog gamer")
     res = clento.query(message.content) 
     answer = next(res.results).text  
-    mssg = answer.replace("Wolfram|Alpha"/"Nathan")
     await message.channel.send("poge")
-    await message.channel.send(answer.replace("Wolfram|Alpha"/"Nathan"))
+    await message.channel.send(answer)
     await message.channel.send("pog")
     for ele in greet:
         if ele in message.content.lower() or ele.strip() == message.content.lower():

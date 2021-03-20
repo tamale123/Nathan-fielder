25915f25b9301df97bc8703b0a4e495e8316d44d
tamale123 <61068363+tamale123@users.noreply.github.com>
2021-03-19 20:16:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import os
 import wolframalpha
 app_id = "9HHRP3-7PAP33XWTU"
@@ -17,49 +18,51 @@ ness = ["💖💖💖","ness 💖","sayori is a fukndofndigbf wbdbfdsbsbNBDASJFB
 4: they main ness
 5: they main ness""","god I love ness","ness is the best","I wish ness live forever","ness is a epic pog gamer","ness looks so epic and cool with his red hat","ness is really great but i dont see whats so great about sayori. he should be with a nice guy like me."]
 @client.event
+
 async def on_message(message):
+    mention = f'<@!{bot.user.id}>'    
+    unless message.channel.id == "822621156951457854" or mention in message.content:
+        return
     if message.author == client.user:
-            return
-    
+        return
     async with message.channel.typing():
-        mention = f'<@!{bot.user.id}>'
-        if mention in message.content:
-            try:
-                res = clento.query(message.content.replace(mention,"")) 
-                answer = next(res.results).text  
-                mssg = (answer.replace("Wolfram|Alpha","Nathan"))
-            except:
-                pass
+        messagecontent = message.content.replace(mention,"")
+        try:
+            res = clento.query(messagecontent) 
+            answer = next(res.results).text  
+            mssg = (answer.replace("Wolfram|Alpha","Nathan"))
+        except:
+            pass
         try:
             for ele in greet:
-                if ele in message.content.lower() or ele.strip() == message.content.lower():
+                if ele in messagecontent.lower() or ele.strip() == messagecontent.lower():
                     mssg=random.choice(greet)
         except:
             pass
         try:
             for ele in name:
-                if ele in message.content.lower() or ele.strip() == message.content.lower():
-                    mssg=message.content.lower().replace(ele,(str(message.author).split("#", 1)[0]))
+                if ele in messagecontent.lower() or ele.strip() == messagecontent.lower():
+                    mssg=messagecontent.lower().replace(ele,(str(message.author).split("#", 1)[0]))
         except:
             pass
         try:
-            msg = message.content.lower().replace("'","")
+            msg = messagecontent.lower().replace("'","")
             if "im " in msg:
                 mssg = 'Hi '+(msg[(msg.find("im ")+3):])+", I'm dad."
         except:
             pass
         try:
-            if "ness" in message.content.lower():
+            if "ness" in messagecontent.lower():
                 mssg=random.choice(ness)    
         except:
             pass
         try:
-            if "no u" in message.content:
+            if "no u" in messagecontent:
                 mssg="no u"
         except:
             pass
         try:
-            if "frog" in message.content.lower():
+            if "frog" in messagecontent.lower():
                 await message.channel.send("<:zgredo:821222638609104916> <:imgay:821222638529150976>")
                 mssg="this frog is gay."
         except:
